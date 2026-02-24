d6076cdd2582faaa70e84fd04957e1aa9e9d5254
arindu20232946-oss <arindu.20232946@iit.ac.lk>
2026-02-24 11:43:52 +0530

    feat: Add progress routes to the API

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,8 +2,10 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
+const progressRoutes = require('./routes/progress-routes');
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/progress', progressRoutes);
 
 export default app;
